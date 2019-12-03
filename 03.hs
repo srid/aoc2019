9b20b94e962e65ad950ce3eb1467eb1771a5f1c3
Sridhar Ratnakumar <srid@srid.ca>
2019-12-03 14:01:40 -0500

    some refactor

--- 03.hs
+++ 03.hs
@@ -29,52 +29,55 @@ data Move
   | D Int
   deriving (Eq, Show)
 
-type Coord = (Int, Int)
+type Point = (Int, Int)
 
--- | A path is set of coordinates and the signal delay length (sum of steps)
-type Path = Map Coord Natural
+-- | A path is map of points to their signal delay length
+type Path = Map Point Natural
 
-distance :: Coord -> Natural
+distance :: Point -> Natural
 distance (a, b) = abs' a + abs' b
 
-moveLength :: Move -> Natural
-moveLength = intToNatural . \case
-  R n -> n
-  U n -> n
-  L n -> n
-  D n -> n
-
-moveCoord :: Coord -> Move -> Coord
-moveCoord (x, y) = bimap (+ x) (+ y) . \case
+pointFromOrigin :: Move -> Point
+pointFromOrigin = \case
   R n -> (n, 0)
   U n -> (0, - n)
   L n -> (- n, 0)
   D n -> (0, n)
 
+pointFrom :: Point -> Move -> Point
+pointFrom p = shiftPoint p . pointFromOrigin
+
+moveLength :: Move -> Natural
+moveLength = distance . pointFromOrigin
+
+shiftPoint :: Point -> Point -> Point
+shiftPoint (x, y) = bimap (+ x) (+ y)
+
 movePath :: Move -> Path
-movePath = \case
-  R n -> coords [0 .. n] [0]
-  U n -> coords [0] [0, -1 .. (- n)]
-  L n -> coords [0, -1 .. (- n)] [0]
-  D n -> coords [0] [0 .. n]
+movePath = points . bimapBoth range . pointFromOrigin
   where
-    coords x y = Map.fromList $ liftA2 (,) x y <&> id &&& distance
+    range 0 = [0]
+    range b = [0, signum b .. b]
+    points (xs, ys) =
+      Map.fromList $ fmap (id &&& distance) $ liftA2 (,) xs ys
 
 mkPath :: [Move] -> Path
-mkPath = snd . foldl' f (((0, 0), 0), mempty)
+mkPath = Map.delete origin . snd . foldl' f ((origin, 0), mempty)
   where
+    origin = (0, 0)
     f ((loc, dist), acc) move =
-      ( (moveCoord loc move, dist + moveLength move),
+      ( (pointFrom loc move, dist + moveLength move),
         Map.union acc $ shiftPath loc dist (movePath move)
       )
-    shiftPath (x, y) dist = Map.mapKeys (bimap (+ x) (+ y)) . Map.map (+ dist)
+    shiftPath (x, y) dist =
+      Map.mapKeys (bimap (+ x) (+ y)) . Map.map (+ dist)
 
 main :: IO ()
 main = do
-  input <- readFileText "input/3"
-  forM_ (samples <> [(input, (280, 10554))]) $ \(inp, (part1Ans, part2Ans)) -> do
+  input <- (,(280, 10554)) <$> readFileText "input/3"
+  forM_ (samples <> [input]) $ \(inp, (part1Ans, part2Ans)) -> do
     let (pathA, pathB) = bimapBoth mkPath $ parseInput inp
-    let common = Map.delete ((0, 0)) $ Map.intersectionWith (+) pathA pathB
+    let common = Map.intersectionWith (+) pathA pathB
     let part1 = minimumElem $ distance <$> Map.keys common
         part2 = minimumElem $ Map.elems common
     putStrLn "Answer:"
@@ -82,11 +85,8 @@ main = do
     print $ assert (part2 == part2Ans) part2
   where
     samples =
-      [ -- 6
-        ("R8,U5,L5,D3\nU7,R6,D4,L4", (6, 30)),
-        -- 159
+      [ ("R8,U5,L5,D3\nU7,R6,D4,L4", (6, 30)),
         ("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", (159, 610)),
-        -- 135
         ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", (135, 410))
       ]
 
@@ -107,9 +107,10 @@ inputParser = (,) <$> (p <* eol) <*> p
     p = sepBy1 moveParser (char ',')
 
 moveParser :: Parser Move
-moveParser = choice $
-  dirs <&> \(c, mk) -> do
-    void $ char c
-    mk <$> L.decimal
+moveParser =
+  choice $
+    dirs <&> \(c, mk) -> do
+      void $ char c
+      mk <$> L.decimal
   where
     dirs = [('R', R), ('U', U), ('L', L), ('D', D)]
